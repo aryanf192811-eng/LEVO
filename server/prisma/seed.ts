a4dffa13716b94f11db6931ab904a314d24d34fe
Ganpati kumar <aryanf192811@gmail.com>
2026-07-12 10:41:26 +0530

    fix(seed): use namespace import for bcryptjs (CommonJS compat)

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient, UserRole, VehicleStatus, DriverStatus, TripStatus, MaintenanceStatus, NotificationType } from '@prisma/client';
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
